5aea8c80ed4beb327f343954a7efdff5ad72ccd7
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-11-05 12:05:44 -0600

    other pages footer and nav

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
+import './About.css'
 
 const About = () => {
   return (
     <div>
       <Nav />
+      <div className="about">
+
+      </div>
       <Footer />
     </div>
   )
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import './Videos.css'
+import Footer from '../components/Footer'
 
 const Videos = () => {
   return (
     <div>
       <Nav />
+      <div className="videos">
+        
+      </div>
       <Footer />
     </div>
   )
