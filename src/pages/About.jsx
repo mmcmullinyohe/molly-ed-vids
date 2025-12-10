c9a3fdc5a5f732a40d80970360522c0eff5bf609
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-12-10 12:16:15 -0600

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,18 +1,53 @@
-import React from 'react'
-import Nav from '../components/Nav'
-import Footer from '../components/Footer'
-import './About.css'
+import React from "react";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+import "./About.css";
 
 const About = () => {
   return (
     <div>
       <Nav />
-      <div className="about">
 
-      </div>
-     <Footer />
+      <main className="about">
+        <h1>Hello, I'm Molly!</h1>
+
+        <p>
+          I have been an educator for 14 years and recently transitioned out of
+          the classroom. I have experience teaching early childhood, elementary,
+          middle school math, and high school math. I love teaching reading and
+          helping students learn to love reading.
+        </p>
+
+        <p>
+          My current teaching certificate includes early childhood (pre-K
+          through 3rd grade), elementary (1st grade through 6th grade), and
+          intermediate level mathematics (middle school and high school up to
+          Algebra 2). Please reach out to me for more information.
+        </p>
+
+        <p className="about-rates">
+          I offer online tutoring through Google Meet at a rate of{" "}
+          <strong>$40 per hour</strong> or <strong>$20 per half hour</strong>{" "}
+          for the following:
+        </p>
+
+        <ul className="about-list">
+          <li>Elementary Reading & Language Arts</li>
+          <li>Elementary Math</li>
+          <li>Elementary Science</li>
+          <li>Elementary Social Studies</li>
+          <li>Pre-Algebra</li>
+          <li>Algebra 1</li>
+          <li>Algebra 2</li>
+          <li>Geometry</li>
+          <li>Homework Help</li>
+        </ul>
+
+      </main>
+
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
