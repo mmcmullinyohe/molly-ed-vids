0fc17a8c4b958836e8a01942cfa85be40f20a9bf
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-12-10 14:17:44 -0600

    update nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../assets/MM_logo_simple.png";
-import './Nav.css'
+import "./Nav.css";
 
 function Nav() {
   return (
     <nav className="navbar">
       <img src={logo} alt="MM Logo" className="nav_logo" />
       <ul className="nav_links">
-        <li className="nav__link"><a href="/">Home</a></li>
-        <li><a href="/booking">Schedule Now</a></li>
-        <li><a href="/videos">Videos</a></li>
-        <li><a href="/about">About</a></li>
-                <li>
+        <li className="nav__link">
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/booking">Schedule Now</Link>
+        </li>
+        <li>
+          <Link to="/videos">Videos</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
           <a href="mailto:mmcmullinyohe@gmail.com">Contact</a>
         </li>
       </ul>
@@ -20,3 +29,4 @@ function Nav() {
 }
 
 export default Nav;
+
