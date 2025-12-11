a58641be292847e4717e5221b5af895735749556
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-12-11 08:18:16 -0600

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";   // ⬅️ Add this import
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <div>
       <div className="footer__container">
-          <ul className="footer__links">
-            <li className="footer__link">
-              <a href="/">Home</a>
-            </li>
-            <li><a href="/booking">Schedule Now</a></li>
-            <li>
-              <a href="/videos">Videos</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-          </ul>
+        <ul className="footer__links">
+          <li className="footer__link">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="footer__link">
+            <Link to="/booking">Schedule Now</Link>
+          </li>
+          <li className="footer__link">
+            <Link to="/videos">Videos</Link>
+          </li>
+          <li className="footer__link">
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
